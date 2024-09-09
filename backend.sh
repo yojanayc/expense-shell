69e93dd3d649c241a4420aad74cfcd13a962efc4
yojanayc <yojanasajja01@gmail.com>
2024-09-09 15:11:06 -0500

    expense-shell

---
+++ backend.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+# echo "Please enter DB password:"
+# read -s mysql_root_password
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+
+dnf module disable nodejs -y &>>LOGFILE
+VALIDATE $? "Disable nodejs module"
+
+dnf module enable nodejs:20 -y &>>LOGFILE
+VALIDATE $? "Enable nodejs module"
+
+dnf install nodejs -y &>>LOGFILE
+VALIDATE $? "Install nodejs module"
+
--- mysql.sh
+++ mysql.sh
@@ -38,5 +38,15 @@ VALIDATE $? "Enabling the mysql server"
 systemctl start mysqld &>>LOGFILE
 VALIDATE $? "Starting the mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
-VALIDATE $? "Setting up the root password"
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+# VALIDATE $? "Setting up the root password"
+
+
+mysql -h db.daws78s.fun -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
