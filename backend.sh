e11d9f22a820f27516026a39d58746f34a2c8d89
yojanayc <yojanasajja01@gmail.com>
2024-09-09 15:38:19 -0500

    expense-shell

--- backend.sh
+++ backend.sh
@@ -77,7 +77,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h db.daws78s.fun -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h mysql.daws78s.fun -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
