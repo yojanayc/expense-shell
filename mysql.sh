e54f470cae27acab711ef95874cf4eab05801272
yojanayc <yojanasajja01@gmail.com>
2024-09-09 15:16:47 -0500

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Starting MySQL Server"
 # VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.daws78s.fun -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
