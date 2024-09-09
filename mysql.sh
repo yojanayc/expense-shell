e710899dd539b1cb88f9e35a6049fb7b8a6baed9
yojanayc <yojanasajja01@gmail.com>
2024-09-09 13:36:51 -0500

    expense-shell

---
+++ mysql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Enter DB password: "
+read -s mysql_root_password
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql-server -y&>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling the mysql server"
+
+systemctl start mysqld &>>LOGFILE
+VALIDATE $? "Starting the mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+VALIDATE $? "Setting up the root password"
